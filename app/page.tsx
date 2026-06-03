26dabdac9e5c2b31554572924a83ab21d1d1356e
Sherlyn <tiwarisherlyn@gmail.com>
2026-06-03 23:58:57 +0530

    Completed Todo App

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,157 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState<any[]>([]);
+
+  useEffect(() => {
+    const savedTodos = localStorage.getItem("todos");
+
+    if (savedTodos) {
+      setTodos(JSON.parse(savedTodos));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(
+      "todos",
+      JSON.stringify(todos)
+    );
+  }, [todos]);
+
+  function addTodo() {
+    if (!task.trim()) return;
+
+    setTodos([
+      ...todos,
+      {
+        text: task,
+        completed: false,
+      },
+    ]);
+
+    setTask("");
+  }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="container">
+      <h1>Your To Do</h1>
+
+      <div className="input-area">
+        <input
+          id="todo-input"
+          type="text"
+          placeholder="Add new task"
+          value={task}
+          onChange={(e) =>
+            setTask(e.target.value)
+          }
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              addTodo();
+            }
+          }}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+
+        <button
+          id="add-btn"
+          onClick={addTodo}
+        >
+          +
+        </button>
+      </div>
+
+      <ul id="todo-list">
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <div className="left">
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => {
+                  const updated = [...todos];
+
+                  updated[index].completed =
+                    !updated[index]
+                      .completed;
+
+                  setTodos(updated);
+                }}
+              />
+
+              <span
+                className={
+                  todo.completed
+                    ? "completed"
+                    : ""
+                }
+              >
+                {todo.text}
+              </span>
+            </div>
+
+            <button
+              className="edit-btn"
+              onClick={() => {
+                const newText = prompt(
+                  "Edit todo:",
+                  todo.textz
+                );
+
+                if (
+                  newText &&
+                  newText.trim()
+                ) {
+                  const updated = [
+                    ...todos,
+                  ];
+
+                  updated[index].text =
+                    newText.trim();
+
+                  setTodos(updated);
+                }
+              }}
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              ✎
+            </button>
+
+            <button
+              className="delete-btn"
+              onClick={() => {
+                setTodos(
+                  todos.filter(
+                    (_, i) =>
+                      i !== index
+                  )
+                );
+              }}
             >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+              ×
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <div className="bottom-text">
+        <h3>
+          Your remaining todos :
+          {
+            todos.filter(
+              (todo) =>
+                !todo.completed
+            ).length
+          }
+        </h3>
+
+        <p>
+          "Doing what you love is the
+          cornerstone of having abundance
+          in your life." - Wayne Dyer
+        </p>
+      </div>
     </div>
   );
 }
